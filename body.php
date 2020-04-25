2f950b546489ad13749cc11bf36ca09ffbbfeb9e
AbhishekKumarSingh00 <51472345+AbhishekKumarSingh00@users.noreply.github.com>
2020-04-25 17:03:35 +0530

    Update body.php

--- body.php
+++ body.php
@@ -10,17 +10,22 @@
     <div class="carousel-inner">
 
       <div class="item active">
-        <img src="img/banner3.jpg" alt="Los Angeles" style="width:100%;">
+        <img src="img/banner5.jpg" alt="Los Angeles" style="width:100%;">
         
       </div>
 
       <div class="item">
-        <img src="img/banner2.jpg" style="width:100%;">
+        <img src="img/banner4.jpg" style="width:100%;">
+        
+      </div>
+	
+       <div class="item">
+        <img src="img/banner3.jpg" style="width:100%;">
         
       </div>
     
       <div class="item">
-        <img src="img/banner4.jpg" alt="New York" style="width:100%;">
+        <img src="img/banner2.jpg" alt="New York" style="width:100%;">
         
       </div>
       <div class="item">
@@ -28,7 +33,7 @@
         
       </div>
       <div class="item">
-        <img src="img/banner3.jpg" alt="New York" style="width:100%;">
+        <img src="img/banner5.jpg" alt="New York" style="width:100%;">
         
       </div>
   
